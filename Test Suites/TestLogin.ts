c6bbdc4d8562483257f3634a5a78fe18db57094b
Aryawan Wijaya <nikolaus.wijaya@si.ukdw.ac.id>
2020-03-03 18:36:20 +0700

    initial commit

---
+++ Test Suites/TestLogin.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Menguji login dengan data yang benar dan salah</description>
+   <name>TestLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e48a10ee-ddd3-477a-8a1c-7544e0834a14</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce343b89-ef76-4766-90b8-83fca4e4d853</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Login Dian dan Kopi</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f59eded8-1207-46ff-acdd-dbf49350e57f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Login Dians dan Kopi</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
