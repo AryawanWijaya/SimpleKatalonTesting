7e2f6f3604eb83380fdc6ab86bf078e1a2447566
Aryawan Wijaya <nikolaus.wijaya@si.ukdw.ac.id>
2020-03-17 17:52:26 +0700

    waiting for element clickable_kuliah online1_minggu7

---
+++ Object Repository/Page_CURA Healthcare Service/body_CURA Healthcare                       _9d7cae.rs
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_CURA Healthcare                       _9d7cae</name>
+   <tag></tag>
+   <elementGuidId>1370ccf4-dfe7-408f-b6fd-e096dbbbcc67</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+
+
+    
+        
+        
+            CURA Healthcare
+        
+        
+            Home
+        
+                
+            History
+        
+        
+            Profile
+        
+        
+            Logout
+        
+            
+
+
+
+
+    
+        CURA Healthcare Service
+        We Care About Your Health
+        
+        Make Appointment
+    
+
+
+
+    
+        
+            
+                Make Appointment
+                
+            
+            
+                
+                    Facility
+                    
+                        
+                            Tokyo CURA Healthcare Center
+                            Hongkong CURA Healthcare Center
+                            Seoul CURA Healthcare Center
+                        
+                    
+                
+                
+                    
+                        
+                             Apply for hospital readmission
+                        
+                    
+                
+                
+                    Healthcare Program
+                    
+                        
+                             Medicare
+                        
+                        
+                             Medicaid
+                        
+                        
+                             None
+                        
+                    
+                
+                
+                    Visit Date (Required)
+                    
+                        
+                            
+                            
+                                
+                            
+                        
+                    
+                
+                
+                    Comment
+                    
+                        
+                    
+                
+                
+                    
+                        Book Appointment
+                    
+                
+            
+        
+    
+
+
+
+
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2020
+            
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+/html[1]/body[1]/div[@class=&quot;datepicker datepicker-dropdown dropdown-menu datepicker-orient-left datepicker-orient-top&quot;]/div[@class=&quot;datepicker-days&quot;]/table[@class=&quot;table-condensed&quot;]/tbody[1]/tr[6]/td[@class=&quot;day&quot;]«March 2020»SuMoTuWeThFrSa23242526272829123456789101112131415161718192021222324252627282930311234TodayClear«2020»JanFebMarAprMayJunJulAugSepOctNovDecTodayClear«2020-2029»201920202021202220232024202520262027202820292030TodayClear«2000-2090»199020002010202020302040205020602070208020902100TodayClear«2000-2900»190020002100220023002400250026002700280029003000TodayClear</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Basic.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Basic test suite for CURA</description>
+   <name>Basic</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>89c0f86b-ebc4-4596-b80b-49b9db0eba51</testSuiteGuid>
+   <testCaseLink>
+      <guid>a94e8ae3-1cfb-4b72-a2df-c160a4626baa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Minggu7_waiting</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
